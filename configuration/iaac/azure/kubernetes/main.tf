54021541845f0bbd7c492b6394c198d4847052de
Bilal-ashlar <bilal@ashlarglobal.com>
2024-09-13 17:02:29 +0500

    new

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -25,9 +25,9 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   default_node_pool {
     name            = "agentpool"
     node_count      = var.node_count
-   # vm_size         = "standard_d2s_v3"
-   # vm_size         = "standard_d2as_v4"      #CHANGE IF AN ERROR ARISES 
-    vm_size	     = "standard_D4as_v4"
+    vm_size         = "standard_ds1_v2"
+   # vm_size         = "standard_d2s_v3"      #CHANGE IF AN ERROR ARISES 
+   # vm_size	     = "standard_D4as_v4"
   }
 
   service_principal {
