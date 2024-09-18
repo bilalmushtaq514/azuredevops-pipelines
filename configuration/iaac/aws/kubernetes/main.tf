cc812b7a41f4f18d6ed681329dcacad50cd60460
Bilal-ashlar <bilal@ashlarglobal.com>
2024-09-18 18:15:56 +0500

    nodes change

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -17,8 +17,11 @@ resource "aws_default_vpc" "default" {
 
 }
 
-data "aws_subnet_ids" "subnets" {
-  vpc_id = aws_default_vpc.default.id
+data "aws_subnets" "subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
 }
 
 provider "kubernetes" {
